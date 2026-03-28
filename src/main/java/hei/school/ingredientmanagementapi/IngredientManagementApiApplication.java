c23b0d275f41bb3fe1992bada7ccb6b78f4d4896
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-28 20:02:52 +0300

    chore: initial project setup with Spring Boot

---
+++ src/main/java/hei/school/ingredientmanagementapi/IngredientManagementApiApplication.java
@@ -0,0 +1,13 @@
+package hei.school.ingredientmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientManagementApiApplication.class, args);
+    }
+
+}
